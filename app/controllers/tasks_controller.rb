a58c6c1f36fa78ae333827b58440336611fcd34d
Xozi <khozaima.mahmoodi@gmail.com>
2025-06-16 18:50:46 +0330

    Implement Task creation (new and create actions), add form and validation error display

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,25 @@ class TasksController < ApplicationController
    def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = 'Task was successfully created.'
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private # متدهای private فقط در داخل همین کلاس قابل دسترسی هستند
+
+  def task_params
+    params.require(:task).permit(:title, :completed) # پارامترهای مجاز برای Task رو تعریف می کنه
+  end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -16,4 +16,55 @@ RSpec.describe TasksController, type: :controller do
       expect(assigns(:tasks)).to eq([task1, task2])
     end
   end
+
+  describe 'GET #new' do
+    it 'returns a success response' do
+      get :new
+      expect(response).to be_successful
+    end
+
+    it 'assigns a new task as @task' do
+      get :new
+      expect(assigns(:task)).to be_a_new(Task)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'creates a new Task' do
+        expect {
+          post :create, params: { task: attributes_for(:task) }
+        }.to change(Task, :count).by(1)
+      end
+
+      it 'redirects to the created task' do
+        post :create, params: { task: attributes_for(:task) }
+        expect(response).to redirect_to(Task.last)
+      end
+
+      it 'sets a flash notice' do
+        post :create, params: { task: attributes_for(:task) }
+        expect(flash[:notice]).to eq 'Task was successfully created.'
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not save the new task' do
+        expect {
+          post :create, params: { task: attributes_for(:task, title: nil) }
+        }.to_not change(Task, :count)
+      end
+
+      it 'returns a success response (i.e. to display the "new" template)' do
+        post :create, params: { task: attributes_for(:task, title: nil) }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to render_template(:new)
+      end
+
+      it 'does not set a flash notice' do
+        post :create, params: { task: attributes_for(:task, title: nil) }
+        expect(flash[:notice]).to be_nil
+      end
+    end
+  end
 end
