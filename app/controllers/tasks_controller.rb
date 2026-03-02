3bec03bb0e1bf0bee9d5352b3c9afa761e4e916c
Xozi <khozaima.mahmoodi@gmail.com>
2026-03-02 18:36:25 +0400

    Improve UI with Tailwind and secure task access by user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,70 +1,65 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
 
-  # GET /tasks or /tasks.json
+  # GET /tasks
   def index
+    # Fetches only tasks belonging to the current logged-in user
     @tasks = current_user.tasks
   end
 
-  # GET /tasks/1 or /tasks/1.json
+  # GET /tasks/1
   def show
   end
 
   # GET /tasks/new
   def new
-    @task = Task.new
+    # Builds a new task associated with the current user
+    @task = current_user.tasks.build
   end
 
   # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks or /tasks.json
+  # POST /tasks
   def create
+    # Initializes a new task through the user relationship
     @task = current_user.tasks.build(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to @task, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
+  # PATCH/PUT /tasks/1
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to @task, notice: "Task was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
+  # DELETE /tasks/1
   def destroy
     @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tasks_url, notice: "Task was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.expect(task: [ :title, :description, :status ])
-    end
+  # Enhanced security for fetching tasks
+  def set_task
+    # This prevents users from accessing other users' tasks by manually changing IDs in the URL
+    @task = current_user.tasks.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to tasks_path, alert: "Access denied: You do not have permission to access this task."
+  end
+
+  # Only allow a list of trusted parameters through
+  def task_params
+    params.expect(task: [ :title, :description, :status ])
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,7 +269,7 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -313,4 +313,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.clean_up_passwords_on_registration = false
 end
